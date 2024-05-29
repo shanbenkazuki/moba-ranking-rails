94b26a25ded77c1886c6d49b985eda6a3ee45ac9
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-05-29 19:41:53 +0900

    tierのバリデーションでnilを許可するように修正

--- app/models/pokemon_tier.rb
+++ app/models/pokemon_tier.rb
@@ -3,5 +3,5 @@ class PokemonTier < ApplicationRecord
 
   validates :pokemon_id, presence: true
   validates :rates_fetched_date, presence: true
-  validates :tier, inclusion: { in: ['S+', 'S', 'A+', 'A', 'B', 'C', 'EX'] }
+  validates :tier, inclusion: { in: ['S+', 'S', 'A+', 'A', 'B', 'C', 'EX'] }, allow_nil: true
 end
