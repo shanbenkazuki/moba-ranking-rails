b1f1a9ea2729b22c46db8777f1f0710a864b0f25
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-05-30 01:33:52 +0900

    OGPを設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      site: 'moba-ranking',
+      title: 'MOBA Tier Rankings',
+      description: 'MOBA関連のTierをランキング形式で公開しています。',
+      canonical: request.original_url,
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        url: request.original_url,
+        image: image_url('default-ogp.png'),
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@あなたのツイッターアカウント',
+        title: :title,
+        description: :description,
+        image: image_url('default-ogp.png'),
+      }
+    }
+  end
 end
