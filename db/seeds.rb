63691d3601dcaa1fb05cf37d37a7d0799e95d9f4
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-05-29 23:12:25 +0900

    EXポケモンをtrueにする

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,49 +57,61 @@ require 'csv'
 #   )
 # end
 
-CSV.foreach(Rails.root.join('db/csv/pokemons.csv'), headers: true) do |row|
-  Pokemon.create!(
-    name_en: row['name_en'],
-    name_jp: row['name_jp'],
-    style: row['style'],
-    tier_img_url: row['tier_img_url']
-  )
-end
+# CSV.foreach(Rails.root.join('db/csv/pokemons.csv'), headers: true) do |row|
+#   Pokemon.create!(
+#     name_en: row['name_en'],
+#     name_jp: row['name_jp'],
+#     style: row['style'],
+#     tier_img_url: row['tier_img_url']
+#   )
+# end
 
-csv_file_path = 'db/csv/pokemon_rates/pokemon_stats_2024-05-14_150906.csv'
-reference_date = Date.parse('2024-05-14')
+# csv_file_path = 'db/csv/pokemon_rates/pokemon_stats_2024-05-14_150906.csv'
+# reference_date = Date.parse('2024-05-14')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  pokemon = Pokemon.find_by(name_en: row['Pokemon'])
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   pokemon = Pokemon.find_by(name_en: row['Pokemon'])
   
-  win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
-  pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
-  ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
+#   win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
+#   pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
+#   ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
   
-  PokemonRate.create!(
-    pokemon_id: pokemon.id,
-    win_rate: win_rate,
-    pick_rate: pick_rate,
-    ban_rate: ban_rate,
-    reference_date: reference_date
-  )
-end
+#   PokemonRate.create!(
+#     pokemon_id: pokemon.id,
+#     win_rate: win_rate,
+#     pick_rate: pick_rate,
+#     ban_rate: ban_rate,
+#     reference_date: reference_date
+#   )
+# end
 
-csv_file_path = 'db/csv/pokemon_rates/pokemon_stats_2024-05-20_171878.csv'
-reference_date = Date.parse('2024-05-20')
+# csv_file_path = 'db/csv/pokemon_rates/pokemon_stats_2024-05-20_171878.csv'
+# reference_date = Date.parse('2024-05-20')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  pokemon = Pokemon.find_by(name_en: row['Pokemon'])
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   pokemon = Pokemon.find_by(name_en: row['Pokemon'])
   
-  win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
-  pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
-  ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
+#   win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
+#   pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
+#   ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
   
-  PokemonRate.create!(
-    pokemon_id: pokemon.id,
-    win_rate: win_rate,
-    pick_rate: pick_rate,
-    ban_rate: ban_rate,
-    reference_date: reference_date
-  )
+#   PokemonRate.create!(
+#     pokemon_id: pokemon.id,
+#     win_rate: win_rate,
+#     pick_rate: pick_rate,
+#     ban_rate: ban_rate,
+#     reference_date: reference_date
+#   )
+# end
+
+ex_pokemon_names = ["MewtwoX", "MewtwoY", "Zacian", "Miraidon"]
+
+ex_pokemon_names.each do |name|
+  pokemon = Pokemon.find_by(name_en: name)
+  if pokemon
+    pokemon.update(is_EX: true)
+    puts "Updated #{name}'s is_EX to true"
+  else
+    puts "Pokemon not found: #{name}"
+  end
 end
