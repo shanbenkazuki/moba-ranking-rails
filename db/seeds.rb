330b234c4de8ca0d3846003338aec2cd7ed4ecab
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-06-10 00:08:43 +0900

    入れてあったやつは削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,126 +9,126 @@
 #   end
 require 'csv'
 
-CSV.foreach(Rails.root.join('db/csv/hero.csv'), headers: true) do |row|
-  Hero.create!(
-    name_en: row['name_en'],
-    name_jp: row['name_jp'],
-    role: row['role'],
-    tier_img_url: row['tier_img_url']
-  )
-end
+# CSV.foreach(Rails.root.join('db/csv/hero.csv'), headers: true) do |row|
+#   Hero.create!(
+#     name_en: row['name_en'],
+#     name_jp: row['name_jp'],
+#     role: row['role'],
+#     tier_img_url: row['tier_img_url']
+#   )
+# end
 
-csv_file_path = 'db/csv/hero_rates/hero_rates_2024-06-07.csv'
-reference_date = Date.parse('2024-06-07')
+# csv_file_path = 'db/csv/hero_rates/hero_rates_2024-06-07.csv'
+# reference_date = Date.parse('2024-06-07')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  HeroRate.create!(
-    hero_id: Hero.find_by(name_en: row['hero']).id,
-    win_rate: row['win_rate'],
-    pick_rate: row['pick_rate'],
-    ban_rate: row['ban_rate'],
-    reference_date: reference_date
-  )
-end
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   HeroRate.create!(
+#     hero_id: Hero.find_by(name_en: row['hero']).id,
+#     win_rate: row['win_rate'],
+#     pick_rate: row['pick_rate'],
+#     ban_rate: row['ban_rate'],
+#     reference_date: reference_date
+#   )
+# end
 
-csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-26.csv'
-reference_date = Date.parse('2024-05-26')
+# csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-26.csv'
+# reference_date = Date.parse('2024-05-26')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  HeroRate.create!(
-    hero_id: Hero.find_by(name_en: row['hero']).id,
-    win_rate: row['win_rate'],
-    pick_rate: row['pick_rate'],
-    ban_rate: row['ban_rate'],
-    reference_date: reference_date
-  )
-end
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   HeroRate.create!(
+#     hero_id: Hero.find_by(name_en: row['hero']).id,
+#     win_rate: row['win_rate'],
+#     pick_rate: row['pick_rate'],
+#     ban_rate: row['ban_rate'],
+#     reference_date: reference_date
+#   )
+# end
 
-csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-19.csv'
-reference_date = Date.parse('2024-05-19')
+# csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-19.csv'
+# reference_date = Date.parse('2024-05-19')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  HeroRate.create!(
-    hero_id: Hero.find_by(name_en: row['hero']).id,
-    win_rate: row['win_rate'],
-    pick_rate: row['pick_rate'],
-    ban_rate: row['ban_rate'],
-    reference_date: reference_date
-  )
-end
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   HeroRate.create!(
+#     hero_id: Hero.find_by(name_en: row['hero']).id,
+#     win_rate: row['win_rate'],
+#     pick_rate: row['pick_rate'],
+#     ban_rate: row['ban_rate'],
+#     reference_date: reference_date
+#   )
+# end
 
-csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-14.csv'
-reference_date = Date.parse('2024-05-14')
+# csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-14.csv'
+# reference_date = Date.parse('2024-05-14')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  HeroRate.create!(
-    hero_id: Hero.find_by(name_en: row['hero']).id,
-    win_rate: row['win_rate'],
-    pick_rate: row['pick_rate'],
-    ban_rate: row['ban_rate'],
-    reference_date: reference_date
-  )
-end
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   HeroRate.create!(
+#     hero_id: Hero.find_by(name_en: row['hero']).id,
+#     win_rate: row['win_rate'],
+#     pick_rate: row['pick_rate'],
+#     ban_rate: row['ban_rate'],
+#     reference_date: reference_date
+#   )
+# end
 
-csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-08.csv'
-reference_date = Date.parse('2024-05-08')
+# csv_file_path = 'db/csv/hero_rates/hero_rates_2024-05-08.csv'
+# reference_date = Date.parse('2024-05-08')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  HeroRate.create!(
-    hero_id: Hero.find_by(name_en: row['hero']).id,
-    win_rate: row['win_rate'],
-    pick_rate: row['pick_rate'],
-    ban_rate: row['ban_rate'],
-    reference_date: reference_date
-  )
-end
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   HeroRate.create!(
+#     hero_id: Hero.find_by(name_en: row['hero']).id,
+#     win_rate: row['win_rate'],
+#     pick_rate: row['pick_rate'],
+#     ban_rate: row['ban_rate'],
+#     reference_date: reference_date
+#   )
+# end
 
-CSV.foreach(Rails.root.join('db/csv/pokemons.csv'), headers: true) do |row|
-  Pokemon.create!(
-    name_en: row['name_en'],
-    name_jp: row['name_jp'],
-    style: row['style'],
-    tier_img_url: row['tier_img_url']
-  )
-end
+# CSV.foreach(Rails.root.join('db/csv/pokemons.csv'), headers: true) do |row|
+#   Pokemon.create!(
+#     name_en: row['name_en'],
+#     name_jp: row['name_jp'],
+#     style: row['style'],
+#     tier_img_url: row['tier_img_url']
+#   )
+# end
 
-csv_file_path = 'db/csv/pokemon_rates/pokemon_rates_2024-05-14_150906.csv'
-reference_date = Date.parse('2024-05-14')
+# csv_file_path = 'db/csv/pokemon_rates/pokemon_rates_2024-05-14_150906.csv'
+# reference_date = Date.parse('2024-05-14')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  pokemon = Pokemon.find_by(name_en: row['Pokemon'])
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   pokemon = Pokemon.find_by(name_en: row['Pokemon'])
   
-  win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
-  pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
-  ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
+#   win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
+#   pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
+#   ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
   
-  PokemonRate.create!(
-    pokemon_id: pokemon.id,
-    win_rate: win_rate,
-    pick_rate: pick_rate,
-    ban_rate: ban_rate,
-    reference_date: reference_date
-  )
-end
+#   PokemonRate.create!(
+#     pokemon_id: pokemon.id,
+#     win_rate: win_rate,
+#     pick_rate: pick_rate,
+#     ban_rate: ban_rate,
+#     reference_date: reference_date
+#   )
+# end
 
-csv_file_path = 'db/csv/pokemon_rates/pokemon_rates_2024-05-20_171878.csv'
-reference_date = Date.parse('2024-05-20')
+# csv_file_path = 'db/csv/pokemon_rates/pokemon_rates_2024-05-20_171878.csv'
+# reference_date = Date.parse('2024-05-20')
 
-CSV.foreach(csv_file_path, headers: true) do |row|
-  pokemon = Pokemon.find_by(name_en: row['Pokemon'])
+# CSV.foreach(csv_file_path, headers: true) do |row|
+#   pokemon = Pokemon.find_by(name_en: row['Pokemon'])
   
-  win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
-  pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
-  ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
+#   win_rate = row['win_rate'] == '-' ? nil : row['win_rate']
+#   pick_rate = row['pick_rate'] == '-' ? nil : row['pick_rate']
+#   ban_rate = row['ban_rate'] == '-' ? nil : row['ban_rate']
   
-  PokemonRate.create!(
-    pokemon_id: pokemon.id,
-    win_rate: win_rate,
-    pick_rate: pick_rate,
-    ban_rate: ban_rate,
-    reference_date: reference_date
-  )
-end
+#   PokemonRate.create!(
+#     pokemon_id: pokemon.id,
+#     win_rate: win_rate,
+#     pick_rate: pick_rate,
+#     ban_rate: ban_rate,
+#     reference_date: reference_date
+#   )
+# end
 
 csv_file_path = 'db/csv/pokemon_rates/pokemon_rates_2024-05-28_160525.csv'
 reference_date = Date.parse('2024-05-28')
