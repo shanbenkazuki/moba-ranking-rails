b874795123561419cf1f40c78781391c2cdc16d7
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-06-14 01:55:10 +0900

    ロールごとで検索できるように修正

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -1,9 +1,13 @@
 class HeroesController < ApplicationController
   def index
     @latest_fetched_date = HeroTier.maximum(:rates_fetched_date)
-    @heroes = Hero.select('heroes.tier_img_url, hero_tiers.tier, heroes.name_en')
-                  .joins(:hero_tiers)
-                  .where(hero_tiers: { rates_fetched_date: @latest_fetched_date })
-                  .order('heroes.name_en')
+    @roles = Hero.distinct.pluck(:role)
+    
+    @q = Hero.ransack(params[:q])
+    @heroes = @q.result
+                .select('heroes.tier_img_url, hero_tiers.tier, heroes.name_en')
+                .joins(:hero_tiers)
+                .where(hero_tiers: { rates_fetched_date: @latest_fetched_date })
+                .order('heroes.name_en')
   end
 end
--- app/models/hero.rb
+++ app/models/hero.rb
@@ -6,4 +6,8 @@ class Hero < ApplicationRecord
     validates :name_jp, presence: true
     validates :role, presence: true, inclusion: { in: ['Fighter', 'Mage', 'Tank', 'Assassin', 'Marksman', 'Support'] }
     validates :tier_img_url, presence: true
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["role"]
+    end
 end
