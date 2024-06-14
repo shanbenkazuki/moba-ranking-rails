50391becb05c6ca656e6ff6232561dcba810f9ca
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-06-14 23:50:29 +0900

    ポケモンのスタイルでtierの検索を可能にした

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,9 +1,14 @@
 class PokemonsController < ApplicationController
   def index
     @latest_fetched_date = PokemonTier.maximum(:rates_fetched_date)
-    @pokemons = Pokemon.select('pokemons.tier_img_url, pokemon_tiers.tier, pokemons.name_en')
-                       .joins(:pokemon_tiers)
-                       .where(pokemon_tiers: { rates_fetched_date: @latest_fetched_date })
-                       .order('pokemons.name_en')
+    @styles = Pokemon.distinct.pluck(:style)
+
+    @q = Pokemon.ransack(params[:q])
+
+    @pokemons = @q.result
+                    .select('pokemons.tier_img_url, pokemon_tiers.tier, pokemons.name_en')
+                    .joins(:pokemon_tiers)
+                    .where(pokemon_tiers: { rates_fetched_date: @latest_fetched_date })
+                    .order('pokemons.name_en')
   end
 end
