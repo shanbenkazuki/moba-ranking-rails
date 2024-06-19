a7227b5acd19789c5dd4abe36564c175f4b564e2
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-06-19 19:42:29 +0900

    ポケモン用のbarchartグラフを追加

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -10,5 +10,9 @@ class PokemonsController < ApplicationController
                     .joins(:pokemon_tiers)
                     .where(pokemon_tiers: { rates_fetched_date: @latest_fetched_date })
                     .order('pokemons.name_en')
+
+    @pokemon_win_rates = PokemonRate.where(reference_date: @latest_fetched_date).joins(:pokemon).order(win_rate: :desc).pluck('pokemons.name_jp', :win_rate)
+    @pokemon_pick_rates = PokemonRate.where(reference_date: @latest_fetched_date).joins(:pokemon).order(pick_rate: :desc).pluck('pokemons.name_jp', :pick_rate)
+    @pokemon_ban_rates = PokemonRate.where(reference_date: @latest_fetched_date).joins(:pokemon).order(ban_rate: :desc).pluck('pokemons.name_jp', :ban_rate)
   end
 end
